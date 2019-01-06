51a8577d8311f862b8ed728216e332bc9bcd0c96
pofider <jan.blaha@hotmail.com>
2019-01-06 21:10:32 +0100

    initial commit

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using jsreport.AspNetCore;
+using jsreport.Types;
+using WebApp.Model;
+using System.Net.Http;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [MiddlewareFilter(typeof(JsReportPipeline))]
+        public IActionResult Invoice()
+        {
+            HttpContext.JsReportFeature()
+                // the normal jsreport base url injection into the html doesn't work properly with docker and asp.net because of port mapping
+                // the project typically starts with some http://localhost:1234 url but inside docker the url is http://localhost
+                .Configure((req) => req.Options.Base = "http://localhost")
+                .Recipe(Recipe.ChromePdf);         
+
+            return View(InvoiceModel.Example());
+        }    
+    }
+}
---
+++ WebApp/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using jsreport.AspNetCore;
+using jsreport.Binary.Linux;
+using jsreport.Local;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace jsreport_dotnet_example_docker
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddJsReport(new LocalReporting()
+                .UseBinary(JsReportBinary.GetBinary())
+                .Configure((cfg) => {
+                    // explicitly set port, because azure web app sets environment variable PORT
+                    // which is used also by jsreport
+                    cfg.HttpPort = 1000;
+                    return cfg;
+                })
+                .AsUtility()
+                .Create());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }          
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
